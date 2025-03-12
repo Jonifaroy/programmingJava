26aca29064ff883ab142fee11bea52c34d82f4f4
Jonifaroy <jonifa510510.com>
2025-03-12 14:37:38 +0530

    programs

---
+++ Methods/method.java
@@ -0,0 +1,16 @@
+public class method
+{
+    public void add(int x, int y)
+    {
+        int z=x+y;
+        System.out.println(z);
+
+    }
+    public static void main(String args[])
+    {
+        
+        method obj=new method();
+        obj.add(10,11);
+
+    }
+}
