a29eeae3f0af243fa5f6776cb2b77f42138a67c7
Jonifaroy <jonifa510510.com>
2025-03-13 10:48:42 +0530

    This a Statement

---
+++ conditionalStatement/Statement.java
@@ -0,0 +1,16 @@
+public class Statement
+{
+    public void main(String args[])
+    {
+        if(true)
+        {
+            System.out.println("The condition is true");
+        
+        }
+        if(false)
+        {
+            System.out.println("The condition is not true");
+            
+        }
+    }
+}
