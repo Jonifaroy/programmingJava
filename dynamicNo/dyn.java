62015b91592aa5ab9ba04d3cfe622119dbe7aa85
Jonifaroy <jonifa510510.com>
2025-03-13 15:27:22 +0530

    This is a dynamic file

---
+++ dynamicNo/dyn.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class dyn {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter your number:");
+        int c= sc.nextInt();
+        System.out.println("Enter the 2nd number");
+        int d=sc.nextInt();
+        int e=c+d;
+        System.out.println(e);
+    }
+}
