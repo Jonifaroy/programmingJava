99ab6c31a8932a0b6359a4954aab8926bac3f406
Jonifaroy <jonifa510510.com>
2025-03-13 11:14:03 +0530

    This a Statement

---
+++ positiveNegative/positive.java
@@ -0,0 +1,15 @@
+public class positive
+{
+    public static void main(String args[])
+    {
+        int num=10;
+        if(num<0)
+        {
+            System.out.println("It is a positive num");
+        }
+        else
+        {
+            System.out.println("It is a negative num");
+        }
+    }
+}
