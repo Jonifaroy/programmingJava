ad0deb43828a1c3d7091a0023ce90b3fa06cb33a
Jonifaroy <jonifa510510.com>
2025-03-13 11:02:56 +0530

    This a Statement

---
+++ ifElse/Else.java
@@ -0,0 +1,16 @@
+public class Else
+{
+    public static void main(String args[])
+    {
+        if(true)
+        {
+            System.out.println("It is true");
+
+        }
+        else
+        {
+            System.out.println("It is false");
+        }
+    }
+
+}
