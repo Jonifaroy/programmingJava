17d1583fda47496e5c6dc7531a172bcb3da3eb23
Jonifaroy <jonifa510510.com>
2025-03-13 14:38:36 +0530

    This is a leap file

---
+++ leapYear/Leap.java
@@ -0,0 +1,16 @@
+public class Leap
+{
+    public static void main(String args[])
+    {
+        int i=2020;
+        if(i%4==0)
+        {
+            System.out.println("It is a leap year");
+        }
+        else 
+        {
+            System.out.println("Not a leap year");
+        }
+    }
+}
+
