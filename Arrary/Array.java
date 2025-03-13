2539b1f350ae689556b350e6e7e65a1886ac52b0
Jonifaroy <jonifa510510.com>
2025-03-13 13:25:34 +0530

    This is a array file

---
+++ Arrary/Array.java
@@ -0,0 +1,10 @@
+public class Array
+{
+    public static void main(String args[])
+    {
+        String fav[]={"doremon","shizhuka","Nobita"};
+        System.out.println(fav[0]);
+        System.out.println(fav[1]);
+        System.out.println(fav[2]);
+    }
+}
