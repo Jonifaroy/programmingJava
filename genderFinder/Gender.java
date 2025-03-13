c9e10c9f80d9a1313248492b6d6767c16cc99e7c
Jonifaroy <jonifa510510.com>
2025-03-13 11:40:02 +0530

    This a Statement for gender

---
+++ genderFinder/Gender.java
@@ -0,0 +1,15 @@
+public class Gender
+{
+    public static void main(String args[])
+    {
+        char gen='F';
+        if(gen=='M')
+        {
+            System.out.println("He is a male");
+        }
+        else
+        {
+            System.out.println("She is a female");
+        }
+    }
+}
