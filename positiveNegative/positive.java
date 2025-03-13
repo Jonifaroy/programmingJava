efdf50df3bd5e7fbc958d50152b2586f65a4b480
Jonifaroy <jonifa510510.com>
2025-03-13 11:45:07 +0530

    this is a positive or negative msg

--- positiveNegative/positive.java
+++ positiveNegative/positive.java
@@ -2,10 +2,14 @@ public class positive
 {
     public static void main(String args[])
     {
-        int num=10;
+        int num=0; 
         if(num<0)
         {
             System.out.println("It is a positive num");
+        }
+        else if(num==0)
+        {
+            System.out.println("It is a nuetral num");
         }
         else
         {
