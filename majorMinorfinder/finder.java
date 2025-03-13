a4e92a60c3146ee5d74b9efb2c7f775b4122a4d6
Jonifaroy <jonifa510510.com>
2025-03-13 11:09:23 +0530

    This a Statement

---
+++ majorMinorfinder/finder.java
@@ -0,0 +1,15 @@
+public class finder
+{
+    public static void main(String args[])
+    {
+        int Age=10;
+        if(Age>=18)
+        {
+            System.out.println("He or she is major");
+        }
+        else
+        {
+            System.out.println("He or she is minor");
+        }
+    }
+}
