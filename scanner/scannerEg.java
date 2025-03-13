0c47be05307e0da7e76d3da722bbaa23ec89e525
Jonifaroy <jonifa510510.com>
2025-03-13 15:14:12 +0530

    This is a scanner file

---
+++ scanner/scannerEg.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class scannerEg {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter your name:");
+        String name = scanner.nextLine();
+        System.out.println(name);
+    }
+}
