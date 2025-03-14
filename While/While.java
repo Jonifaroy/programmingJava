cf0eae64b495f2ed8379cffd874293b6e0da1c8a
Jonifaroy <jonifa510510.com>
2025-03-14 10:04:22 +0530

    It is a while loop

---
+++ While/While.java
@@ -0,0 +1,13 @@
+public class While
+{
+    public static void main(String args[])
+    {
+        int i=92;
+        while(i<99)
+        {
+            System.out.println(i);
+            i=i+1;
+        }
+    }
+
+}
