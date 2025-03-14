806e83a434f586a333e4c898516598cdd0d10efa
Jonifaroy <jonifa510510.com>
2025-03-14 09:36:45 +0530

    It is a positive negative finder

---
+++ posiNeg/pos.java
@@ -0,0 +1,17 @@
+public class pos
+{
+    public static void main(String args[])
+    {
+        int num=0; 
+        num=1;
+        if(num>0)
+        {
+            System.out.println("It is a positive num");
+        }
+        
+        else
+        {
+            System.out.println("It is a negative num");
+        }
+    }
+}
