83f6f92d43bc0fe7155b5d8d44849af2ead55fce
Jonifaroy <jonifa510510.com>
2025-03-14 09:50:28 +0530

    It is a command

---
+++ Command/Commanding.java
@@ -0,0 +1,8 @@
+public class Commanding
+{
+    public static void main(String args[])
+    {
+        int num=10;    //It is a number
+        System.out.println(num);/* printing the values*/
+    }
+}
