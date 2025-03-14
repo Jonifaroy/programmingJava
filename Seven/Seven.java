75fa89acc5d49b0e551864504c42579879456ad1
Jonifaroy <jonifa510510.com>
2025-03-14 10:13:50 +0530

    It is a while loop

---
+++ Seven/Seven.java
@@ -0,0 +1,15 @@
+public class Seven
+{
+    public static void main(String args[])
+    {
+        int i=7;
+        int x=1;
+        while(i<=71)
+        {
+            System.out.println(x+"x 7="+i);
+            x=x+1;
+            i=i+7;
+        }
+
+    }
+}
