171dfe1fa119ea621d61ae7e765841f691dfec1e
Jonifaroy <jonifa510510.com>
2025-03-14 09:43:52 +0530

    It is a command

--- Commands/file.java
+++ Commands/file.java
@@ -2,7 +2,7 @@ public class file
 {
     public static void main(String args[])
     {
-        int num=10;    //it is a number
+        int num=10;    
         System.out.println(num);/* printing the values*/
     }
 }
