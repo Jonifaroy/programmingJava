985a009d904b2850bd4918a5efdaf6bb56ac0fb5
Jonifaroy <jonifa510510.com>
2025-03-15 10:39:25 +0530

    Constructor

---
+++ constructor/Constructor.java
@@ -0,0 +1,11 @@
+public class Constructor
+{
+    Constructor()
+    {
+        System.out.println("Hey Nobita");
+    }
+    public static void main(String args[])
+    {
+         Constructor a= new Constructor();
+    }
+}
