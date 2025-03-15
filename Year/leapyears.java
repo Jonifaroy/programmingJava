cf867587d279c3be178de7da4e68e34b3b38bbc1
Jonifaroy <jonifa510510.com>
2025-03-15 10:25:37 +0530

    It is a leap year

---
+++ Year/leapyears.java
@@ -0,0 +1,16 @@
+public class leapyears
+{
+    public static void main(String args[])
+    {
+        for(int i=2020;i<=2030;i=i+1)
+        {
+            if(i%4==0)
+            {
+                System.out.println(i);
+
+            }
+           
+        }
+    }
+}
+
